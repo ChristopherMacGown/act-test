23a5c762dfaf94ea71aca5241ec17e77c634fbdc
[bot] rosie <rosie@drill.run>
2023-05-24 22:55:02 +0000

    Automatic update with format changes from linters.
      * black
      * ruff
    
    Signed-off-by: [bot] rosie <rosie@drill.run>

--- foo.py
+++ foo.py
@@ -1,14 +1,16 @@
 import foo
 import bar
+
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 
 
 foo.boop
+
 
 def foo(bar: None):
     bar
 
 
 if __name__ == "__main__":
-            bar.baz    
-            print(foo(None))
+    bar.baz
+    print(foo(None))
