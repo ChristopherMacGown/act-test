1c3068375213c1b14a3e30c4d1ef23bcfdb87402
ChristopherMacGown <ChristopherMacGown@users.noreply.github.com>
2023-05-25 22:00:31 +0000

    Automatic update with format changes from linters.

--- foo.py
+++ foo.py
@@ -1,5 +1,6 @@
-import foo
 import bar
+
+import foo
 
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 
@@ -12,5 +13,5 @@ def foo(bar: None):
 
 
 if __name__ == "__main__":
-        bar.baz
-        print(foo(None))
+    bar.baz
+    print(foo(None))
